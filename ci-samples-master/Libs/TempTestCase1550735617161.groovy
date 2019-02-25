4f769bf6ae6083b55f9edc1d33a288da27fb3a97
namph2501 <43846316+namph2501@users.noreply.github.com>
2019-02-25 14:14:04 +0700

    [ci/cd] integrate source code to github

---
+++ ci-samples-master/Libs/TempTestCase1550735617161.groovy
@@ -0,0 +1,23 @@
+import com.kms.katalon.core.main.TestCaseMain
+import com.kms.katalon.core.logging.KeywordLogger
+import groovy.lang.MissingPropertyException
+import com.kms.katalon.core.testcase.TestCaseBinding
+import com.kms.katalon.core.driver.internal.DriverCleanerCollector
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.webui.contribution.WebUiDriverCleaner
+import com.kms.katalon.core.mobile.contribution.MobileDriverCleaner
+import com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner
+
+
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.webui.contribution.WebUiDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.mobile.contribution.MobileDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner())
+
+
+RunConfiguration.setExecutionSettingFile('/var/folders/6q/1p24rnh50vx6cw7jy5701trc0000gp/T/Katalon/Test Cases/Common Test Cases/Login/20190221_145337/execution.properties')
+
+TestCaseMain.beforeStart()
+
+        TestCaseMain.runTestCase('Test Cases/Common Test Cases/Login', new TestCaseBinding('Test Cases/Common Test Cases/Login',[:]), FailureHandling.STOP_ON_FAILURE , false)
+    
